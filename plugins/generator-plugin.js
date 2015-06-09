a0b346985f4c033877a223cfc6989692911fd3b6
Jurgen Leschner <jldec@ciaosoft.com>
2015-06-09 19:23:05 -0400

    credit, better mobile unclick on hover

--- plugins/generator-plugin.js
+++ plugins/generator-plugin.js
@@ -6,61 +6,45 @@ module.exports = function(generator) {
 
   hb.registerHelper('navIcon', function(frame) {
 
-    return defaultFragmentHtml('/#navicon',
+    return hb.defaultFragmentHtml('/#navicon',
       '_!bars fw lg_',
       '<span class="icon">=</span>',
       frame);
   });
 
   hb.registerHelper('docTitle', function(frame) {
-    var title = opts.docTitle ||
-      (opts.pkgJson && opts.pkgJson.name) ||
-      'pub-server';
-
+    var title = opts.docTitle || opts.pkgName || 'pub-server';
     var subTitle = opts.docSubTitle || '';
 
-    return defaultFragmentHtml('/#doctitle',
+    return hb.defaultFragmentHtml('/#doctitle',
       '# [' + title + '](/)\n' + subTitle,
       '<h1><a href="/">' + u.escape(title) + '</a></h1>' + subTitle,
       frame);
   });
 
   hb.registerHelper('topMenu', function(frame) {
-    var url = opts.github || githubUrl();
+    var url = opts.github || hb.githubUrl();
 
-    return defaultFragmentHtml('/#topmenu',
+    return hb.defaultFragmentHtml('/#topmenu',
       url ? '- [_!github lg fw_](' + url + ' "github")' : '',
       url ? '<ul><li><a href="' + url + '">github</a></li></ul>' : '',
       frame);
   });
 
+  hb.registerHelper('credit', function(frame) {
+    if (opts.credit || !('credit' in opts)) {
+      var credit = opts.credit ||
+        'powered by ' +
+        '[pub-server](https://github.com/jldec/pub-server)' +
+        (opts.theme ? ' and [' + opts.theme.pkgName + '](' +
+          hb.githubUrl(opts.theme.pkgJson) + ')' : '');
 
-  // try user-provided fragment, then faMarkdown with font-awesome, then html
-  function defaultFragmentHtml(fragmentName, faMarkdown, html, frame) {
-
-    var f = generator.fragment$[fragmentName];
-    if (f) return fragmentHtml(f, frame);
-
-    if (faMarkdown && u.find(opts.pkgs, function(pkg) {
-      return /pub-pkg-font-awesome$/i.test(
-        (pkg.pkgJson && pkg.pkgJson.name) || pkg.dir);
-    })) {
-      return fragmentHtml( {_txt:faMarkdown,
-        _href:'/#synthetic' }, frame, {noWrap:1});
+      return hb.defaultFragmentHtml(
+        '/#credit',
+        '_!heart_ ' + credit,
+        '',
+        frame)
     }
+  });
 
-    return html;
-  }
-
-  function fragmentHtml(fragment, frame, opts) {
-    return generator.renderHtml(fragment, hb.renderOpts(frame, opts));
-  }
-
-  function githubUrl() {
-    var url = opts.pkgJson && opts.pkgJson.repository && opts.pkgJson.repository.url;
-    var match;
-    if (url && (match = url.match(/git:\/\/(github\.com.*)\.git$/i))) {
-      return 'https://' + match[1];
-    }
-  }
 }
