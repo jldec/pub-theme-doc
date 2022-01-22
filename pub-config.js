bbe065a9bfe0c5d0322d7fd3513fa67adc18f7f3
jldec <jurgen@ciaosoft.com>
2022-01-22 10:32:39 -0500

    v1.1.15 bump deps - use github-markdown-light

--- pub-config.js
+++ pub-config.js
@@ -4,7 +4,7 @@ module.exports =
   generatorPlugins: './plugins/generator-plugin.js',
   sources:          './templates',
   staticPaths: [
-    { path:'./node_modules/github-markdown-css/github-markdown.css', route:'/css', inject:true },
+    { path:'./node_modules/github-markdown-css/github-markdown-light.css', route:'/css', inject:true },
     { path:'./css/pub-theme-doc.css', route:'/css', inject:true }
   ]
 }
